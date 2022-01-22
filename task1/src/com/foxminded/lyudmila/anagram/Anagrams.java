43575b049245728ead77830f26b3a88a5cfcc901
Lyudmila Ivanyuk <sony_2004@mail.ru>
2022-01-22 21:39:38 +0700

    Updated classes Anagrams and Main

--- task1/src/com/foxminded/lyudmila/anagram/Anagrams.java
+++ task1/src/com/foxminded/lyudmila/anagram/Anagrams.java
@@ -15,18 +15,17 @@ public class Anagrams {
 		int start = 0;
 		int end = word.length() - 1;
 		while (start < end) {
-			while (!Character.isLetter(word.charAt(start)) && start < end) {
+			if (!Character.isLetter(word.charAt(start))) {
 				start += 1;
-			}
-			while (!Character.isLetter(word.charAt(end)) && start < end) {
+			} else if (!Character.isLetter(word.charAt(end))) {
+				end -= 1;
+			} else {
+				final char letter = word.charAt(start);
+				word.setCharAt(start, word.charAt(end));
+				word.setCharAt(end, letter);
+				start += 1;
 				end -= 1;
 			}
-			final char letter = word.charAt(start);
-			word.setCharAt(start, word.charAt(end));
-			word.setCharAt(end, letter);
-			start += 1;
-			end -= 1;
-
 		}
 		return word.toString();
 	}
--- task1/src/com/foxminded/lyudmila/anagram/Main.java
+++ task1/src/com/foxminded/lyudmila/anagram/Main.java
@@ -3,36 +3,13 @@ package com.foxminded.lyudmila.anagram;
 import java.util.Scanner;
 
 public class Main {
-	public static final String ENTER = "enter";
-	public static final String TEST = "test";
-	public static final String EXIT = "exit";
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		loop: while (true) {
-			System.out.println("\nAnnagram program.\n");
-			System.out.println("Enter your string or select test:");
-			System.out.println("1. Enter \n" + "2. Test \n" + "3. Exit\n");
-			String action = scanner.nextLine().toLowerCase();
-
-			switch (action) {
-			case ENTER:
-				System.out.println("\nEnter string: ");
-				System.out.println("\nResult: " + new Anagrams().reversesLettersInWords(scanner.nextLine()));
-				break;
-			case TEST:
-				String testText = "abcd1j absd!jh";
-				System.out.println("\nTest: " + testText);
-				System.out.println("\nResult: " + new Anagrams().reversesLettersInWords("abcd1j absd!jh"));
-				break;
-			case EXIT:
-				break loop;
-			default:
-				System.out.println("\nIncorrect enter.");
-				break;
-			}
-		}
+		System.out.println("Enter string:");
+		String string = scanner.nextLine();
+		System.out.println("\nResult: ");
+		System.out.println(new Anagrams().reversesLettersInWords(string));
 		scanner.close();
 	}
 
