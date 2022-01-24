33347bd4971c2d16b13d65e0a80b64d4814cebe3
Lyudmila Ivanyuk <sony_2004@mail.ru>
2022-01-24 17:37:06 +0700

    Correction spaces

--- task1/src/com/foxminded/lyudmila/anagram/Anagrams.java
+++ task1/src/com/foxminded/lyudmila/anagram/Anagrams.java
@@ -1,32 +1,32 @@
 package com.foxminded.lyudmila.anagram;
 
 public class Anagrams {
-	public String reversesLettersInWords(String string) {
-		String[] words = string.split(" ");
-		for (int i = 0; i < words.length; i++) {
-			words[i] = reversesAllLetters(words[i]);
-		}
-		return String.join(" ", words);
+    public String reversesLettersInWords(String string) {
+        String[] words = string.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = reversesAllLetters(words[i]);
+        }
+        return String.join(" ", words);
 
-	}
+    }
 
-	private String reversesAllLetters(String word) {
-		StringBuilder reversesWord = new StringBuilder(word);
-		int start = 0;
-		int end = reversesWord.length() - 1;
-		while (start < end) {
-			if (!Character.isLetter(reversesWord.charAt(start))) {
-				start++;
-			} else if (!Character.isLetter(reversesWord.charAt(end))) {
-				end--;
-			} else {
-				final char letter = reversesWord.charAt(start);
-				reversesWord.setCharAt(start, reversesWord.charAt(end));
-				reversesWord.setCharAt(end, letter);
-				start++;
-				end--;
-			}
-		}
-		return reversesWord.toString();
-	}
+    private String reversesAllLetters(String word) {
+        StringBuilder reversesWord = new StringBuilder(word);
+        int start = 0;
+        int end = reversesWord.length() - 1;
+        while (start < end) {
+            if (!Character.isLetter(reversesWord.charAt(start))) {
+                start++;
+            } else if (!Character.isLetter(reversesWord.charAt(end))) {
+                end--;
+            } else {
+                final char letter = reversesWord.charAt(start);
+                reversesWord.setCharAt(start, reversesWord.charAt(end));
+                reversesWord.setCharAt(end, letter);
+                start++;
+                end--;
+            }
+        }
+        return reversesWord.toString();
+    }
 }
--- task1/src/com/foxminded/lyudmila/anagram/Main.java
+++ task1/src/com/foxminded/lyudmila/anagram/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Enter string:");
-		String string = scanner.nextLine();
-		System.out.println("\nResult: ");
-		System.out.println(new Anagrams().reversesLettersInWords(string));
-		scanner.close();
-	}
+        System.out.println("Enter string:");
+        String string = scanner.nextLine();
+        System.out.println("\nResult: ");
+        System.out.println(new Anagrams().reversesLettersInWords(string));
+        scanner.close();
+    }
 }
